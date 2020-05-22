113fac92763644332ec7abbe9d92ae37da7f2ac9
James Abel <j@abel.co>
2020-05-21 20:36:53 -0700

    just the basics

--- test_pyship/tstpyshipapp/tstpyshipapp/app.py
+++ test_pyship/tstpyshipapp/tstpyshipapp/app.py
@@ -1,6 +1,10 @@
+import json
 import sys
+
+from .__version__ import __application_name__ as name
+from .__version__ import __version__ as version
 
 
 def tstpyshipapp():
-    print("hello app world")
+    print(json.dumps({"name": name, "version": version}, indent=4))
     sys.exit(0)  # OK
