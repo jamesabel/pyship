4fa7093d2c2457867c218f14295e90d9035a10fa
James Abel <j@abel.co>
2020-05-22 01:19:57 -0700

    just the basics

--- test_pyship/test_z_check_app_output.py
+++ test_pyship/test_z_check_app_output.py
@@ -5,8 +5,14 @@ from test_pyship import TST_APP_LAUNCHER_EXE_PATH
 
 
 def test_check_app_output():
+
+    def check_output(p):
+        app_out = json.loads(p.stdout)  # the test app prints out JSON
+        assert app_out["name"] == "tstpyshipapp"
+        assert app_out["version"] == "0.0.1"
+
     # test that the created frozen app can run correctly, including checking its output
     p = subprocess.run(str(TST_APP_LAUNCHER_EXE_PATH), cwd=TST_APP_LAUNCHER_EXE_PATH.parent, capture_output=True, text=True)
-    app_out = json.loads(p.stdout)  # the test app prints out JSON
-    assert app_out["name"] == "tstpyshipapp"
-    assert app_out["version"] == "0.0.1"
+    check_output(p)
+    p = subprocess.run(str(TST_APP_LAUNCHER_EXE_PATH), capture_output=True, text=True)  # check execution from a dir that's not the parent of the .exe
+    check_output(p)
