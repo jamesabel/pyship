78a62115959af4c0ba32118eb932c085afcd9e1e
James Abel <j@abel.co>
2020-05-22 22:23:16 -0700

    just the basics

--- test_pyship/test_z_check_app_output.py
+++ test_pyship/test_z_check_app_output.py
@@ -1,15 +1,20 @@
 import subprocess
 import json
 
+from typeguard import typechecked
+
 from test_pyship import TST_APP_LAUNCHER_EXE_PATH
+from test_pyship.tstpyshipapp.tstpyshipapp import __version__ as tstpyshipapp_version
+from test_pyship.tstpyshipapp.tstpyshipapp import __application_name__ as tstpyshipapp_name
 
 
 def test_check_app_output():
 
-    def check_output(p):
-        app_out = json.loads(p.stdout)  # the test app prints out JSON
-        assert app_out["name"] == "tstpyshipapp"
-        assert app_out["version"] == "0.0.1"
+    @typechecked(always=True)
+    def check_output(check_process: subprocess.CompletedProcess):
+        app_out = json.loads(check_process.stdout)  # the test app prints out JSON
+        assert app_out["name"] == tstpyshipapp_name
+        assert app_out["version"] == tstpyshipapp_version
 
     # test that the created frozen app can run correctly, including checking its output
     p = subprocess.run(str(TST_APP_LAUNCHER_EXE_PATH), cwd=TST_APP_LAUNCHER_EXE_PATH.parent, capture_output=True, text=True)
