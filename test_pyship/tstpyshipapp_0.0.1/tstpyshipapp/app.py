bdd8abac3c660435fe5bae2088583c44fbd080aa
James Abel <j@abel.co>
2020-06-12 17:19:45 -0700

    get updater to work

--- test_pyship/tstpyshipapp_0.0.1/tstpyshipapp/app.py
+++ test_pyship/tstpyshipapp_0.0.1/tstpyshipapp/app.py
@@ -11,14 +11,16 @@ from pyship import __author__ as pyship_author
 from .__init__ import __application_name__ as name
 from .__init__ import __version__ as version
 
-log = get_logger(f"{name}_{version}")
+logger_name = f"{name}_{version}"
+
+log = get_logger(logger_name)
 
 
 def tstpyshipapp():
 
     verbose = len(sys.argv) > 1 and (sys.argv[1].lower() == "-v" or sys.argv[1].lower() == "--verbose")
 
-    balsa = Balsa(name, pyship_author, verbose=verbose)
+    balsa = Balsa(logger_name, pyship_author, verbose=verbose)
     balsa.init_logger()
 
     updater = UpdaterLocal(name)
