3ff613f7fe55bc08f94d364e9326df8bc86c1d22
James Abel <j@abel.co>
2020-06-14 14:07:12 -0700

    fix tests

--- test_pyship/test_a_module_info.py
+++ test_pyship/test_a_module_info.py
@@ -6,5 +6,5 @@ from test_pyship import TST_APP_NAME, TstAppDirs
 
 def test_module_info():
     tst_app_dirs = TstAppDirs(TST_APP_NAME, VersionInfo.parse("0.0.1"))
-    module_info = ModuleInfo(TST_APP_NAME, tst_app_dirs.app_dir)
+    module_info = ModuleInfo(TST_APP_NAME, tst_app_dirs.project_dir)
     print(module_info)
--- test_pyship/test_y_pyship.py
+++ test_pyship/test_y_pyship.py
@@ -1,11 +1,15 @@
+from semver import VersionInfo
+
 from ismain import is_main
 
 from pyship import PyShip
-from test_pyship import TST_APP_PROJECT_DIR
+from test_pyship import TST_APP_NAME, TstAppDirs
 
 
 def test_pyship():
-    py_ship = PyShip(target_app_parent_dir=TST_APP_PROJECT_DIR)
+    version = VersionInfo.parse("0.0.1")
+    tst_app_dirs = TstAppDirs(TST_APP_NAME, version)
+    py_ship = PyShip(app_parent_dir=tst_app_dirs.app_dir)
     py_ship.ship()
 
 
