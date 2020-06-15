8e73a5392e7573b4602b8e4780a7cef997261977
James Abel <j@abel.co>
2020-06-14 19:55:45 -0700

    remove temp test dirs

--- test_pyship/conftest.py
+++ test_pyship/conftest.py
@@ -2,13 +2,16 @@ import pytest
 from pathlib import Path
 import logging
 import sys
+from semver import VersionInfo
 
 from balsa import Balsa
 
-from pyship import mkdirs, subprocess_run, pyship_print
+from pyship import mkdirs, subprocess_run, pyship_print, rmdir
 from pyship import __application_name__ as pyship_application_name
 from pyship import __author__ as pyship_author
 from pyship import __version__ as pyship_version
+
+from test_pyship import TST_APP_NAME, TstAppDirs
 
 
 class TestPyshipLoggingHandler(logging.Handler):
@@ -28,6 +31,13 @@ def session_fixture():
     logging.getLogger().addHandler(test_handler)
 
     balsa.init_logger()
+
+    # remove test app dirs
+    for version_string in ["0.0.1", "0.0.2"]:
+        version = VersionInfo.parse(version_string)
+        tst_app_dirs = TstAppDirs(TST_APP_NAME, version)
+        rmdir(tst_app_dirs.app_dir)
+        rmdir(tst_app_dirs.dist_dir)
 
     # use flit to build pyship into a distributable package in the "dist" directory
     mkdirs(Path("dist"), remove_first=True)
