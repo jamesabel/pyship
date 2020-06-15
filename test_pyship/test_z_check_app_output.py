ffae82c0ee2e1ad72c58bba59484deb3cf72ad33
James Abel <j@abel.co>
2020-06-14 19:18:02 -0700

    fix tests

--- test_pyship/test_z_check_app_output.py
+++ test_pyship/test_z_check_app_output.py
@@ -12,7 +12,7 @@ from test_pyship import TST_APP_NAME
 
 def test_check_app_output():
 
-    version = VersionInfo.parse("0.0.2")
+    version = VersionInfo.parse("0.0.2")  # must be run after other tests that create the test app with this version
 
     @typechecked(always=True)
     def check_output(check_process: subprocess.CompletedProcess):
