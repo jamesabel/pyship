b0e33e89310f594477a81c9412c9eb521f3fe2f3
James Abel <j@abel.co>
2020-07-01 00:32:44 -0700

    don't use flit - use regular setup and twine instead

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
+from pprint import pprint
 from setuptools import setup, find_packages
 
 from pyship import __application_name__, __author__, __version__
 
 requirements = ["setuptools", "wheel", "ismain", "balsa", "requests", "attrs", "typeguard", "toml", "pyinstaller", "semver"]
+
+packages = find_packages(where=__application_name__)
+
+pprint(f"{packages=}")
 
 setup(
     name=__application_name__,
@@ -10,7 +15,11 @@ setup(
     author=__author__,
 
     package_dir={'': __application_name__},
-    packages=find_packages(where=__application_name__),
+    packages=packages,
+
+    package_data={
+        "": ["pyship.ico"],  # does not work todo: fix it
+    },
 
     install_requires=requirements
 )
