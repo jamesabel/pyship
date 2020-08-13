9f4520674bc08a18647173eb51a99579e893db92
James Abel <j@abel.co>
2020-08-13 15:23:43 -0700

    fix author for logging

--- pyship/main.py
+++ pyship/main.py
@@ -1,12 +1,14 @@
-from pyship import PyShip, __application_name__, __version__, PyshipLog, arguments
+from pyship import PyShip, __application_name__, __author__, PyshipLog, arguments, pyship_print
 
 
 def main():
 
     args = arguments()
 
-    pyship_log = PyshipLog(__application_name__, __version__)
+    pyship_log = PyshipLog(__application_name__, __author__)
     pyship_log.init_logger_from_args(args)
+
+    pyship_print(f"log_path={pyship_log.log_path}")
 
     pyship = PyShip()
     if args.name is not None:
