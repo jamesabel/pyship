750795fd9d09311689f6c4aa7143b96268e00ade
James Abel <j@abel.co>
2020-10-13 00:27:06 -0700

    remove dead code

--- test_pyship/conftest.py
+++ test_pyship/conftest.py
@@ -1,19 +1,11 @@
 import pytest
 from pathlib import Path
 import logging
-import os
-import time
-from datetime import timedelta
-from semver import VersionInfo
-import subprocess
 
 from balsa import Balsa
 
-from pyshipupdate import rmdir
 from pyship import __application_name__ as pyship_application_name
 from pyship import __author__ as pyship_author
-
-from test_pyship import TST_APP_NAME, TstAppDirs
 
 
 class TestPyshipLoggingHandler(logging.Handler):
@@ -33,19 +25,3 @@ def session_fixture():
     logging.getLogger().addHandler(test_handler)
 
     balsa.init_logger()
-
-    # create app environments
-    #
-    # subprocess.run(["build.bat"], shell=True)  # update pyship wheel to the latest
-    #
-    # for version_string in ["0.0.1", "0.0.2"]:
-    #     tst_app_dirs = TstAppDirs(TST_APP_NAME, VersionInfo.parse(version_string))
-    #
-    #     rmdir(tst_app_dirs.app_dir)
-    #
-    #     # only recreate the venv infrequently since it's slow and shouldn't have an effect anyway
-    #     #if not tst_app_dirs.venv_dir.exists() or os.path.getmtime(str(tst_app_dirs.venv_dir)) + timedelta(days=1).total_seconds() < time.time():
-    #     # subprocess.run(["make_venv.bat"], cwd=tst_app_dirs.project_dir, shell=True)  # create the venv for the test app
-    #
-    #     rmdir(tst_app_dirs.dist_dir)
-    #     subprocess.run(["build.bat"], cwd=tst_app_dirs.project_dir, shell=True)  # make the dist (wheel) for the test app
