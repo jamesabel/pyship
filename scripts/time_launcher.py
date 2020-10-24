075ffbdbb6d45693bc6c485c7f2c15b131509861
James Abel <j@abel.co>
2020-10-24 11:50:31 -0700

    update launcher benchmark

--- scripts/time_launcher.py
+++ scripts/time_launcher.py
@@ -3,20 +3,19 @@ import time
 import subprocess
 import shutil
 
-from pyship import create_launcher, TargetAppInfo, __application_name__
+from pyship import create_launcher, __application_name__, AppInfo
 
 from ismain import is_main
 
 if is_main():
-    target_app_info = TargetAppInfo()  # picks up pyship info from pyship's pyproject.toml
-    dist_dir = Path("temp", "dist")
-    shutil.rmtree(dist_dir)
-    create_launcher(target_app_info, dist_dir)
-    exe_path = Path(dist_dir, __application_name__, f"{__application_name__}.exe")
+
+    # benchmark how long it takes for the launcher to start up
+
+    exe_path = Path("app", __application_name__, f"{__application_name__}.exe").resolve().absolute()
     print(exe_path)
     for iteration in range(0, 6):
         start = time.time()
-        launcher_process = subprocess.run(exe_path, cwd=dist_dir, capture_output=True)  # the launcher won't actually work, but we're just trying to time it
+        launcher_process = subprocess.run(exe_path, cwd=exe_path.parent, capture_output=True)  # the launcher won't actually work, but we're just trying to time it
         end = time.time()
         print(f"{end - start} sec")
         if iteration == 0:
