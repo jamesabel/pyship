a9615c2df238b0451b7a976d4e3feb1b6d7c6920
James Abel <j@abel.co>
2021-06-07 20:32:14 -0700

    Python 3.9

--- make_venv.bat
+++ make_venv.bat
@@ -1,6 +1,7 @@
 echo on
 rmdir /S /Q venv
-"\Program Files\Python38\python.exe" -m venv --clear venv
+"\Program Files\Python39\python.exe" -m venv --clear venv
 venv\Scripts\python.exe -m pip install --no-deps --upgrade pip
 venv\Scripts\pip3 install -U setuptools
-venv\Scripts\pip3 install -U -r requirements-dev.txt -f ..\pyshipupdate\dist
+REM venv\Scripts\pip3 install -U -r requirements-dev.txt -f ..\pyshipupdate\dist
+venv\Scripts\pip3 install -U -r requirements-dev.txt
