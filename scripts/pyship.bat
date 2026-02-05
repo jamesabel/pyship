a4b545a144c84d5e0670be8bbf9e0ae203d73d7d
James Abel <j@abel.co>
2026-02-05 12:15:30 -0800

    move scripts to the scripts dir (#17)

--- scripts/pyship.bat
+++ scripts/pyship.bat
@@ -1,3 +1,6 @@
+pushd .
+cd ..
 REM make pyship the app
 call local_install.bat
 venv\Scripts\python.exe -m pyship -p bup
+pushd
--- scripts/pytest.bat
+++ scripts/pytest.bat
@@ -1,4 +1,7 @@
+pushd .
+cd ..
 call local_install.bat
 set PYTHONPATH=%CD%
 venv\Scripts\pytest.exe --rootdir="." -s test_pyship
 set PYTHONPATH=
+pushd
