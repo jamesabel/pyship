0e845b8ef0b57bf8850c9b144b78698962370b11
JamesAbel <j@abel.co>
2026-02-19 22:10:21 -0800

    fix make venv

--- example/make_venv.bat
+++ example/make_venv.bat
@@ -1,6 +1,6 @@
 @echo on
 rmdir /S /Q venv
-python -m venv --clear venv
+"C:\Program Files\Python314\python.exe" -m venv --clear venv
 venv\Scripts\python.exe -m pip install --upgrade pip
 venv\Scripts\pip3.exe install -U setuptools
 venv\Scripts\pip3.exe install -U -r requirements.txt -f ..\dist
